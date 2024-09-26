8974e3a6d7b6527ee631f2b7d450773209239911
jayakumar35 <jayakumar53c@gmail.com>
2024-09-26 15:02:39 +0530

    madify commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Form, Button,Table } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function App() {
   const [tasks, setTasks] = useState([]);
